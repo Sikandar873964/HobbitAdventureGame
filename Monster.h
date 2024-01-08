8e1305437b9f6a83ab2f88f05c82650f1caa87d3
Sikandar <sikandarbutt@hotmail.co.uk>
2024-01-08 20:37:26 +0000

    Class player and monster is completed

--- Monster.h
+++ Monster.h
@@ -7,4 +7,21 @@ class Monster : public Character {
 private:
   int roarDmg;
   int strength;
+
+public:
+  Monster() {
+    roarDmg = 0;
+    strength = 10;
+  };
+  void setRoarDmg(int r) { roarDmg = r; }
+  int getRoar() { return roarDmg; }
+  void setStrength(int s) { strength = s; }
+  int getStrength() { return strength; }
+  int UsespecialPower() {
+    if (strength >= 15) {
+      roarDmg = 100;
+    }
+    return roarDmg;
+  }
+  ~Monster() {}
 };
--- Player.h
+++ Player.h
@@ -8,5 +8,31 @@ private:
   int swordDmg;
   int xp; // xp is experience. more experience you gain more powerful you
           // become.
-
+public:
+  Player() : Character() {
+    swordDmg = 0;
+    xp = 0;
+  }
+  Player(int swordDmg, int xp) : Character() {
+    this->swordDmg = swordDmg;
+    this->xp = xp;
+  }
+  void setswordDmg(int dmg) { swordDmg = dmg; }
+  int getswordDmg() { return swordDmg; }
+  void setXp(int e) { xp = e; }
+  int getXp() { return xp; }
+  int UseSpecialPower() {
+    int count = 0;
+    while (count < 3) {
+      if (xp > 5) {
+        swordDmg = swordDmg + xp;
+        xp += 2;
+      } else {
+        swordDmg = 5;
+      }
+      count++;
+    }
+    return swordDmg;
+  }
+  ~Player() {}
 };
